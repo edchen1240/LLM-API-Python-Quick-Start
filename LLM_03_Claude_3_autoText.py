4cd49e90070aad37e72b6f1971000e07e3502984
edchen1240 <106031570+edchen1240@users.noreply.github.com>
2024-03-17 22:04:59 -0400

    Update LLM_03_Claude_3_autoText.py

--- LLM_03_Claude_3_autoText.py
+++ LLM_03_Claude_3_autoText.py
@@ -2,7 +2,7 @@
 [OpenAI-API-1_text.py]
 Purpose: Correct Ask Directory or select file.
 Author: Meng-Chi Ed Chen
-Date: 2023-11-07
+Date: 2024-03-16
 Reference:
     1. 
     2.
@@ -13,7 +13,7 @@ Status: Complete.
 #[1] Import library.
 import LLM_00_module_1_API as APIS #type: ignore
 import LLM_00_module_2_Peripheral as PERF #type: ignore
-gemini_api_key = 'put-your-api-key-here'  #  put-your-api-key-here.
+anthropic_api_key='put-your-api-key-here'#put-your-api-key-here.
 path_prompt = r'D:\01_Floor\a_Ed\09_EECS\10_Python\03_MatureTools\[LLM API]\01_Codes\09_Common Prompts.txt'
 dir_questions = r'D:\01_Floor\a_Ed\09_EECS\10_Python\03_MatureTools\[LLM API]'
 PERF.remove_older_asked_questions(dir_questions, 'txt', '00_Asked questions', day=5)
@@ -21,9 +21,9 @@ max_loop = 10 # The program will run for 10 times before we need to start it aga
 
 
 #[2] Assign model. View more model and pricing here: https://cloud.google.com/vertex-ai/generative-ai/docs/learn/model-versioning
-model_name = 'gemini-1.0-pro-latest'	
-#model_name = 'gemini-1.0-pro'
-#model_name = 'gemini-1.0-pro-001'
+model_name = 'claude-3-opus-20240229'	
+#model_name = 'claude-3-sonnet-20240229'
+#model_name = 'claude-3-haiku-20240307'
 print(f'[Model] {model_name}')
 
 for loop in range(1, max_loop + 1):
@@ -33,11 +33,11 @@ for loop in range(1, max_loop + 1):
     question_content = PERF.read_text_from_txt(path_txt = path_txt, 
                                                 char_preview = 300)
     #[4] Send API request.
-    reply = APIS.DeepMind_Gemini_API_short(model_name = model_name
-                            , gemini_api_key = gemini_api_key
-                            , question_content = question_content
-                            , temperature = 0.9
-                            , max_output_tokens = 5000)
+    reply = APIS.Anthropic_Claude_API_long(model_name, 
+                                            anthropic_api_key, 
+                                            question_content,
+                                            max_tokens = 3000,
+                                            temperature=None)
     
     PERF.append_text_to_txt(path_txt = path_txt
                             , text_to_append = reply
